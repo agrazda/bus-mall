18d2ea007bf5b885b9b130bdef5794fd66460930
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-07-26 22:56:23 -0700

    started site

---
+++ js/app.js
@@ -0,0 +1,43 @@
+'use strict'
+console.log("hello world")
+
+//---------------------Global Variables-------------------------//
+let clickcounter= 0
+const ulElem = document.getElementById('image-clicks');
+const voteSelectionElem = document.getElementById('all_images');
+const leftImageElem = document.getElementById('left_image');
+const leftImageElem = document.getElementById()
+
+
+
+
+//--------------------Constructor Functions----------------------//
+function product(name, imgPath) {
+    this.name =name;
+    this.imgPath=imgPath;
+    this.votes = 0;
+}
+
+
+
+//--------------------Prototype Methods---------------------------//
+
+for
+
+
+
+//----------------------Standard Global Functions----------------//
+
+
+
+
+
+
+//------------------------Listeners-------------------------------//
+
+
+
+
+
+
+//------------------------Call Functions--------------------------//
