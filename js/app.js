6eb4585778556934874aeda5ba4b87aa5eeac5c9
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-07-27 23:27:43 -0700

    adding images

--- js/app.js
+++ js/app.js
@@ -1,43 +1,188 @@
-'use strict'
-console.log("hello world")
+"use strict";
+console.log("hello world");
+
+//Constructor for products
 
 //---------------------Global Variables-------------------------//
-let clickcounter= 0
-const ulElem = document.getElementById('image-clicks');
-const voteSelectionElem = document.getElementById('all_images');
-const leftImageElem = document.getElementById('left_image');
-const leftImageElem = document.getElementById()
+// dom references
+const voteSelectionElem = document.getElementById("vote_selection");
+const leftImageElem = document.getElementById("left_image");
+const leftImageh2Elem = document.getElementById("left_image_h2");
+const centerImageElem = document.getElementById("center_image");
+const centerImageh2Elem = document.getElementById("center_image_h2");
+const rightImageElem = document.getElementById("right_image");
+const rightImageh2Elem = document.getElementById("right_image_h2");
+const ulElem = document.getElementById("image-clicks");
 
+let currentLeft = null;
+let currentRight = null;
+let currentCenter = null;
 
-
+let rounds = 20;
 
 //--------------------Constructor Functions----------------------//
-function product(name, imgPath) {
-    this.name =name;
-    this.imgPath=imgPath;
-    this.votes = 0;
+function Product(name, imgPath) {
+  this.name = name;
+  this.imgPath = imgPath;
+  this.votes = 0;
+  this.views = 0;
+}
+
+Product.allProducts = [];
+
+// ---------------------Prototype Methods------------------------//
+// renders a single image/name
+Product.prototype.renderProduct =function(img, h2) {
+    img.src = this.imgPath;
+    img.alt = this.name;
+    h2.textContent = this.name;
+    this.views++;
+}
+
+//---------------------Standard Global Functions----------------//
+
+//pick 3 unique images, no repeats
+function picksThreeProducts(){
+    const doNotUse =[currentRight, currentLeft, currentCenter];
+    while (doNotUse.includes(currentLeft)){
+        let leftIndex = Math.floor(Math.random()* Product.allProducts.length);
+        currentLeft = Product.allProducts[leftIndex];
+    }
+    doNotUse.push(currentLeft)
+
+    while (doNotUse.includes(currentRight)){
+        let rightIndex = Math.floor(Math.random()* Product.allProducts.length);
+        currentRight = Product.allProducts[rightIndex];
+    }
+    doNotUse.push(currentRight)
+
+    while (doNotUse.includes(currentCenter)){
+        let centerIndex = Math.floor(Math.random()* Product.allProducts.length);
+        currentCenter = Product.allProducts[centerIndex];
+    }
+    doNotUse.push(currentCenter)
+    console.log(doNotUse)
 }
 
 
+//render 3 images
+function renderThreeProducts(){
+    currentCenter.renderProduct(centerImageElem, centerImageh2Elem);
+    currentLeft.renderProduct(leftImageElem, leftImageh2Elem);
+    currentRight.renderProduct(rightImageElem, rightImageh2Elem);
+}
+//render results
 
-//--------------------Prototype Methods---------------------------//
+//click handler
 
-for
+function handleClick(e) {
+    const target = e.target.id;
+    if (target==='left_image' || target === 'right_image' || target === 'center_image') {
+        //updatevoterounds
+        rounds--;
+              //determine their choise and update the votes on the
+        if (target === 'left_image') {
+            currentLeft.clicks++
+        }
+        if (target === 'right_image') {
+            currentRight.clicks++
+        }
+        if (target === 'center_image') {
+            currentCenter.click++
+        }
+
+        picksThreeProducts();
+        renderThreeProducts();
+    }
+        if (rounds === 0) {
+        voteSelectionElem.removeEventListener('click', handleClick);
+        }
+}
+function renderButton
 
 
+//--------------------------Listeners---------------------------//
+//listener on the container for pictures
+voteSelectionElem.addEventListener('click', handleClick);
 
-//----------------------Standard Global Functions----------------//
+//-----------------------Call Functions-------------------------//
 
+Product.allProducts.push(new Product("bag", "./img/assets/bag.jpg"));
+Product.allProducts.push(new Product("banana", "./img/assets/banana.jpg"));
+Product.allProducts.push(new Product("bathroom", "./img/assets/bathroom.jpg"));
+Product.allProducts.push(new Product("boots", "./img/assets/boots.jpg"));
+Product.allProducts.push(new Product("breakfast", "./img/assets/breakfast.jpg"));
+Product.allProducts.push(new Product("bubblegum", "./img/assets/bubblegum.jpg"));
+Product.allProducts.push(new Product("chair", "./img/assets/chair.jpg"));
+Product.allProducts.push(new Product("cthulhu", "./img/assets/cthulhu.jpg"));
+Product.allProducts.push(new Product("dog-duck", "./img/assets/dog-duck.jpg"));
+Product.allProducts.push(new Product("dragon", "./img/assets/dragon.jpg"));
+Product.allProducts.push(new Product("pen", "./img/assets/pen.jpg"));
+Product.allProducts.push(new Product("pet-sweep", "./img/assets/pet-sweep.jpg"));
+Product.allProducts.push(new Product("scissors", "./img/assets/scissors.jpg"));
+Product.allProducts.push(new Product("shark", "./img/assets/shark.jpg"));
+Product.allProducts.push(new Product("sweep", "./img/assets/sweep.png"));
+Product.allProducts.push(new Product("tauntaun", "./img/assets/tauntaun.jpg"));
+Product.allProducts.push(new Product("unicorn", "./img/assets/unicorn.jpg"));
+Product.allProducts.push(new Product("water can", "./img/assets/water-can.jpg"));
+Product.allProducts.push(new Product("wine-glass", "./img/assets/wine-glass.jpg"));
 
+picksThreeProducts();
+renderThreeProducts();
+console.log(Product.allProducts);
 
+//---------------------chart-------------------------------//
+// function makeProductChart() {
+//     var ctx = document.getElementById('').getcontext('');
+//     let productName = [];
+//     let productVotes = [];
+//     let colorArray = [];
 
+//     for   {
+//     productName.push(product.name)
+//     productVotes.push(product.name)
+//     }
 
+//     for (let i = 0; i < product.allProducts.length; i++) {
+//         if(i % 2 === 0) {
+//             colorArray.push('blue');
+//         } else {
+//             colorArray.push('pink')
+//         }
+//     }
 
-//------------------------Listeners-------------------------------//
-
-
-
-
-
-
-//------------------------Call Functions--------------------------//
+// const ctx = document.getElementById('myChart');
+// const myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [colorArray
+//                 // 'rgba(255, 99, 132, 0.2)',
+//                 // 'rgba(54, 162, 235, 0.2)',
+//                 // 'rgba(255, 206, 86, 0.2)',
+//                 // 'rgba(75, 192, 192, 0.2)',
+//                 // 'rgba(153, 102, 255, 0.2)',
+//                 // 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [colorArray
+//                 // 'rgba(255, 99, 132, 1)',
+//                 // 'rgba(54, 162, 235, 1)',
+//                 // 'rgba(255, 206, 86, 1)',
+//                 // 'rgba(75, 192, 192, 1)',
+//                 // 'rgba(153, 102, 255, 1)',
+//                 // 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
